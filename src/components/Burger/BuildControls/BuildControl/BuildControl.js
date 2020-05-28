bc712d07d429525bd1c83bde6b2e60da690a4096
mike10951 <miguel.cabrera.1095@gmail.com>
2020-05-28 14:27:42 -0500

    Ingredients can be added or removed by user and total price is displayed and updated.

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,8 +5,14 @@ import classes from './BuildControl.module.css';
 const buildControl = (props) => (
   <div className={classes.BuildControl}>
     <div className={classes.Label}>{props.label}</div>
-    <button className={classes.Less}>Less</button>
-    <button classNam={classes.More} onClick={props.added}>
+    <button
+      className={classes.Less}
+      onClick={props.removed}
+      disabled={props.disabled}
+    >
+      Less
+    </button>
+    <button className={classes.More} onClick={props.added}>
       More
     </button>
   </div>
